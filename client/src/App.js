5fdee5f55ceda06036d17d79f7b0b476521c76b9
Stephen Moore <judo2000@comcast.net>
2022-07-15 12:59:50 -0600

    Fix api endpoint in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,10 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import Home from './pages/Home';
 
 // Construct our main GraphQL API endpoint
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
 const client = new ApolloClient({
-  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-  //link: authLink.concat(httpLink),
+  uri: '/graphql',
   cache: new InMemoryCache(),
 });
 
