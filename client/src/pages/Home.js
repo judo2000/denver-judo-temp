2d6d2baf21858f972db9800f464e5b07ab0ce5c5
Stephen Moore <judo2000@comcast.net>
2022-07-26 16:26:07 -0600

    Add class, sign-up, and about judo sections to home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,6 +5,7 @@ import { GET_SINGLE_PAGE } from '../utils/queries';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   const { pageTitle } = useParams();
@@ -23,51 +24,130 @@ const Home = () => {
       {loading ? (
         <h2>LOADING...</h2>
       ) : (
-        <div>
-          <Container fluid>
-            <div className='video-box'>Place holder</div>
-            <div className='home-heading text-center py-4'>
-              <h1>Train for competition or self defence at denver judo</h1>
-            </div>
-            <div>
-              <Row>
-                <Col lg-4>
-                  <div className='home-class-heading'>
-                    <h2>Judo Fundamentals</h2>
-                    <span>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Tempora amet temporibus reiciendis delectus, totam atque
-                      nam a ipsa quasi nisi blanditiis doloremque distinctio
-                      minima odit? <a href='class'>Learn more...</a>
-                    </span>
-                  </div>
-                </Col>
-                <Col lg-4>
-                  <div className='home-class-heading'>
-                    <h2>Competition Judo</h2>
-                    <span>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Quis alias laudantium doloremque minus nihil tempora
-                      dignissimos accusamus labore, at, ea debitis aspernatur
-                      eius perferendis.
-                    </span>
-                  </div>
-                </Col>
-                <Col lg-4>
-                  <div className='home-class-heading'>
-                    <h2>Kids Judo</h2>
-                    <span>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Quia nesciunt laboriosam, incidunt perferendis suscipit
-                      esse iste officiis vel quam ipsam voluptatum aliquam dolor
-                      adipisci? Et!
-                    </span>
-                  </div>
-                </Col>
-              </Row>
-            </div>
-          </Container>
-        </div>
+        <Container fluid>
+          <Row>
+            <main>
+              <section id='video'>
+                <div className='video-box'>Place holder</div>
+              </section>
+
+              <section id='home-heading' className='my-4'>
+                <div className='text-center'>
+                  <h1 className='home-heading'>
+                    Train for competition or self defence at denver judo
+                  </h1>
+                </div>
+              </section>
+
+              <section id='classes' className='my-4 py-4'>
+                <div>
+                  <Row>
+                    <Col xs={12} md={4} className='py-2'>
+                      <div>
+                        <h2 className='home-section-heading'>
+                          Judo Fundamentals
+                        </h2>
+                        <span className='home-section-text'>
+                          Lorem ipsum dolor sit, amet consectetur adipisicing
+                          elit. Assumenda eius hic fugit provident officia
+                          suscipit. <Link to='/'>Learn more...</Link>
+                        </span>
+                      </div>
+                    </Col>
+                    <Col xs={12} md={4} className='py-2'>
+                      <div>
+                        <h2 className='home-section-heading'>
+                          Competition Judo
+                        </h2>
+                        <span className='home-section-text'>
+                          Lorem ipsum dolor sit, amet consectetur adipisicing
+                          elit. Nihil, amet. Voluptatum cupiditate magnam enim
+                          sunt! <Link to='/'>Learn more...</Link>
+                        </span>
+                      </div>
+                    </Col>
+                    <Col xs={12} md={4} className='py-2'>
+                      <div>
+                        <h2 className='home-section-heading'>Kids Judo</h2>
+                        <span className='home-section-text'>
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Minima cumque incidunt temporibus excepturi
+                          perspiciatis magni! <Link to='/'>Learn more...</Link>
+                        </span>
+                      </div>
+                    </Col>
+                  </Row>
+                </div>
+              </section>
+
+              <section id='try-a-class' className='mt-4'>
+                <div className='text-center'>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html:
+                        "<iframe id='idZenPlannerFrame' style='width: 80%; height: 500px; ' src='https://denverjudo.zenplanner.com/zenplanner/portal/freeTrial.cfm?type=FreeTrial&amp;FRAME=true' />",
+                    }}
+                  />
+                </div>
+              </section>
+
+              <section id='about-judo'>
+                <div className='pt-5'>
+                  <Row className='mt-4 py-5 justify-content-center'>
+                    <Col md={10}>
+                      <Row>
+                        <Col xs={12} md={6} className='ps-5'>
+                          <div className='ms-5'>
+                            <img
+                              className='home-about-img img-fluid'
+                              src='img/kano.png'
+                              alt='Kano'
+                            />
+                          </div>
+                        </Col>
+                        <Col xs={12} md={6} className='ps-5'>
+                          <div className='mx-4'>
+                            <h2 className='home-section-heading text-white'>
+                              Jigoro Kano
+                            </h2>
+                            <span className='home-section-text text-white'>
+                              Lorem ipsum dolor sit amet consectetur adipisicing
+                              elit. Velit accusamus sequi doloremque. Aut,
+                              nesciunt quod.
+                            </span>
+                          </div>
+                        </Col>
+                      </Row>
+                      <Row className='py-5 '>
+                        <Col xs={12} md={6} className='ps-5'>
+                          <div className='ms-5'>
+                            <h2 className='home-section-heading text-white'>
+                              Jigoro Kano
+                            </h2>
+                            <span className='home-section-text text-white'>
+                              Lorem ipsum dolor sit amet consectetur adipisicing
+                              elit. Velit accusamus sequi doloremque. Aut,
+                              nesciunt quod.
+                            </span>
+                          </div>
+                        </Col>
+                        <Col xs={12} md={6} className='ps-5'>
+                          <div className='mx-5'>
+                            <img
+                              className='home-about-img img-fluid'
+                              src='img/kano.png'
+                              alt='Kano'
+                            />
+                          </div>
+                        </Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                </div>
+              </section>
+            </main>
+          </Row>
+        </Container>
       )}
     </>
   );
